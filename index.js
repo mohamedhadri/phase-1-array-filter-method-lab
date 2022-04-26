0a4bd64221e9d8d231c5e70cf6fb39175a35e0b2
Mohamed Hadri <mhadri1000@gmail.com>
2022-04-26 19:34:56 +0300

    filter arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(driv,drivName){
+return driv.filter((x)=>x.toLowerCase()=== drivName.toLowerCase())
+}
+
+let arr= ['mohamed','hadri','ahmad','hasan']
+
+console.log(findMatching(arr,'hasan'))
+
+function  fuzzyMatch(driv,drivName){
+
+    return driv.filter(
+        (possibleMatch) =>
+          possibleMatch.toLowerCase().indexOf(drivName.toLowerCase()) === 0
+      );
+    }
+
+
+console.log(fuzzyMatch(arr,'h'))
+
+function matchName(source, soughtName) {
+    return source.filter((record) => record.name === soughtName);
+  }
